b944cfed7cf2f9fcf20b08588c993daf3e6c5348
Aditya Choudhary <adityachy02131@gmail.com>
2024-06-26 01:30:13 +0530

    Pattern Exercises First

---
+++ Pattern.py
@@ -0,0 +1,19 @@
+# Triangle Pattern programs
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1, 5):
+#     print(i)
+
+# n = 7
+# for i in range(1, n+1):
+#     print(i)
+
+# n = 7
+# for i in range(1, n+1):
+#     print("*")
+
+n = 7
+for i in range(1, n+1):
+    print(i * "*")
