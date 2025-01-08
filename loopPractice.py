ecd1adc4f1f6a7519664587440595c0ea5e18a1b
adityachodhry <adityachy02131@gmail.com>
2025-01-08 23:04:53 +0530

    For Test

--- loopPractice.py
+++ loopPractice.py
@@ -1,8 +1,8 @@
-# # print 10 to 0
-# for i in range (10, -1, -1):
-#     print(i)
+# print 10 to 0
+for i in range (10, -1, -1):
+    print(i)
 
-# print(f"*"*100)
+print(f"*"*100)
 
 # Print the first 10 natural numbers using for loop.
 for a in range (1, 11):
