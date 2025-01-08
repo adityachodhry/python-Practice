e986dc4a0716ab05d6def653da16a161e009afd1
adityachodhry <adityachy02131@gmail.com>
2025-01-08 23:05:23 +0530

    For Test

--- loopPractice.py
+++ loopPractice.py
@@ -1,12 +1,13 @@
-# print 10 to 0
-for i in range (10, -1, -1):
-    print(i)
+# # print 10 to 0
+# for i in range (10, -1, -1):
+#     print(i)
 
-print(f"*"*100)
+# print(f"*"*100)
 
-# Print the first 10 natural numbers using for loop.
-for a in range (1, 11):
-    print(a)
+# # Print the first 10 natural numbers using for loop.
+# for a in range (1, 11):
+#     print(a)
+
 
 name = str(input("Enter Your Name: "))
 age = int(input("Enter Your Age: "))
