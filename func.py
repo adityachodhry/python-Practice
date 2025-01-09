059fb5f01caa932725cf5e0b39a63774079c65cf
adityachodhry <adityachy02131@gmail.com>
2025-01-10 00:56:41 +0530

    Today Updates

--- func.py
+++ func.py
@@ -1,9 +1,7 @@
-# # 1. map()
+# 1. map()
 
-# # Purpose: Applies a function to all items in an iterable (e.g., list).
+# Purpose: Applies a function to all items in an iterable (e.g., list).
 
-# nums = [1, 2, 3, 4]
-# squared = list(map(lambda x: x ** 2, nums))
-# print(squared)  # Output: [1, 4, 9, 16]
-
-# 2. 
+nums = [1, 2, 3, 4]
+squared = list(map(lambda x: x ** 2, nums))
+print(squared)  # Output: [1, 4, 9, 16]
