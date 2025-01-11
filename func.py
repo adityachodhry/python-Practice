434145f6a9e693c7e269dc43cc01353ec9ede64f
adityachodhry <adityachy02131@gmail.com>
2025-01-11 11:45:32 +0530

    Today Updates

--- func.py
+++ func.py
@@ -1,7 +1,18 @@
-# 1. map()
+# # 1. map()
 
-# Purpose: Applies a function to all items in an iterable (e.g., list).
+# # Purpose: Applies a function to all items in an iterable (e.g., list).
 
-nums = [1, 2, 3, 4]
-squared = list(map(lambda x: x ** 2, nums))
-print(squared)  # Output: [1, 4, 9, 16]
+# nums = [1, 2, 3, 4]
+# squared = list(map(lambda x: x ** 2, nums))
+# print(squared)  # Output: [1, 4, 9, 16]
+
+
+# 2. odd number and even number 
+
+def is_even(number):
+    if number % 2 == 0:
+        print(number, "is Even!")
+    else:
+        print(number, "is Odd!")
+
+is_even(6)
