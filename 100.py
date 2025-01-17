2f16eae84ddfc9cb8f71e994147cfe03ceecabbb
adityachodhry <adityachy02131@gmail.com>
2025-01-18 00:46:31 +0530

    Python program to find the maximum number in a list without using any built-in function or library:

--- 100.py
+++ 100.py
@@ -14,4 +14,4 @@ if number > 999 and number < 10000:
     reversed_number = reverse_number(number)
     is_reverse = check_reverse(number)
     print("Reversed number:", reversed_number)
-    print("Is the reverse true?", is_reverse)    
+    print("Is the reverse true?", is_reverse)   
