3457abf8e750d89aad4c449445e6843c42d64b80
adityachodhry <adityachy02131@gmail.com>
2025-01-19 18:55:46 +0530

    todays updates

--- 100.py
+++ 100.py
@@ -39,3 +39,6 @@ if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         print(year, "is also a Century Year!")
         if year % 400 == 0:
             print(year, "is also a Leap Century Year!")
+
+
+            
