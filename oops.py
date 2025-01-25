9ebbe4e9f1d13e16cf9cb1435f72090ec2776656
adityachodhry <adityachy02131@gmail.com>
2025-01-26 02:02:46 +0530

    Python Questions

---
+++ oops.py
@@ -0,0 +1,18 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_student_info(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+# Create a student object
+
+student1 = Student("John Doe", 18)
+student2 = Student("Jane Smith", 17)
+
+# Display student information
+
+student1.display_student_info()
+student2.display_student_info()
