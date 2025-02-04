b4f13deb09d466bc835aa781e5756242fa25f0b3
adityachodhry <adityachy02131@gmail.com>
2025-02-04 12:35:40 +0530

    Python Practices

---
+++ practices.py
@@ -0,0 +1,32 @@
+# # Find Occurrences in the list
+
+# ls = [10, 2, 6, 10, 5, 10,]
+
+# count = 10
+# x = 0
+
+# for i in ls:
+#     if i == count:
+#         x += 1
+
+# print(f"The number {count} appears {x} times in the list.")
+
+# # Another method to find the Occurrences
+
+# ls = [10, 2, 6, 10, 5, 10,]
+# count = 10
+# print(ls.count(count))
+
+
+# # Find the maximum value in the list
+
+# ls = [10, 2, 6, 10, 5, 10,]
+
+# x = ls[0]
+
+# for i in ls:
+#     if i < x:
+#         x = i
+
+# print(x)
+
