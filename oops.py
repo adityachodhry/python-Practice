9f713b802ee1d5040234b5f5d72134ff867b04cf
adityachodhry <adityachy02131@gmail.com>
2025-02-11 11:39:19 +0530

    Python Practices

--- oops.py
+++ oops.py
@@ -6,19 +6,35 @@
 # Inheritance in Python
 # Data Abstraction in Python
 
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def display_student_info(self):
+#         print("Name:", self.name)
+#         print("Age:", self.age)
+
+# # Create a student object
+# student1 = Student("John Doe", 18)
+# student2 = Student("Jane Smith", 17)
+
+# # Display student information
+# student1.display_student_info()
+# student2.display_student_info()
+
+
+# Inheritance
+
 class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    
+    def __str__(self):
+        return f"Student Name: {self.first_name} {self.last_name}"
 
-    def display_student_info(self):
-        print("Name:", self.name)
-        print("Age:", self.age)
 
-# Create a student object
-student1 = Student("John Doe", 18)
-student2 = Student("Jane Smith", 17)
-
-# Display student information
-student1.display_student_info()
-student2.display_student_info()
+    
+s1 = Student("Aditya", "Choudhary")
+print(s1)
